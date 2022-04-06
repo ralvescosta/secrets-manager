db349382fd7eae3d404c080392d1aa872d031c3c
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-05 21:29:18 -0300

    feat: example

--- example_project/go.mod
+++ example_project/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.18
+
+require github.com/ralvescosta/dotenv v1.0.4 // indirect
--- example_project/main.go
+++ example_project/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/ralvescosta/dotenv"
+)
+
+func main() {
+	dotenv.Configure(".env.development")
+
+	log.Printf("DB HOST: %v \n", os.Getenv("DB_HOST"))
+	log.Printf("DB USER: %v \n", os.Getenv("DB_USER"))
+	log.Printf("DB PASS: %v \n", os.Getenv("DB_PASS"))
+}
