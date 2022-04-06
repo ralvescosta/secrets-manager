c133355d9d360994764c9a2c6097473e80114704
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-06 13:36:30 -0300

    refactor: merge cli

--- vault/cmd/merge.go
+++ vault/cmd/merge.go
@@ -5,7 +5,11 @@ Copyright © 2022 Rafael Costa <rafael.rac.mg@gmail.com>
 package cmd
 
 import (
+	"log"
+
+	"github.com/ralvescosta/secrets-manager/vault/pkg/vault"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 // mergeCmd represents the base command when called without any subcommands
@@ -13,10 +17,13 @@ var mergeCmd = &cobra.Command{
 	Use:   "merge", // Vault Manager
 	Short: "Merge you environment file",
 	Long:  `Example: cli -v https://vault -t validToken`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		configs, err := getVaultConfigs(cmd.Flags())
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		vault.Run(configs)
 	},
 }
 
@@ -30,4 +37,45 @@ func init() {
 	mergeCmd.Flags().StringP("kv-version", "k", "1", "Key Value version")
 	mergeCmd.MarkFlagRequired("file-path")
 	mergeCmd.MarkFlagRequired("token")
+}
+
+func getVaultConfigs(flags *pflag.FlagSet) (*vault.Configs, error) {
+	filePath, err := flags.GetString("file-path")
+	if err != nil {
+		return nil, err
+	}
+
+	vaultSeparator, err := flags.GetString("vault-separator")
+	if err != nil {
+		return nil, err
+	}
+
+	pathKeyValueSeparator, err := flags.GetString("path-key-value-separator")
+	if err != nil {
+		return nil, err
+	}
+
+	kvVersion, err := flags.GetString("kv-version")
+	if err != nil {
+		return nil, err
+	}
+
+	vaultHost, err := flags.GetString("vault-host")
+	if err != nil {
+		return nil, err
+	}
+
+	vaultToken, err := flags.GetString("token")
+	if err != nil {
+		return nil, err
+	}
+
+	fileKeyValueSeparator, err := flags.GetString("file-path")
+	if err != nil {
+		return nil, err
+	}
+
+	return &vault.Configs{
+		filePath, vaultSeparator, pathKeyValueSeparator, kvVersion, vaultHost, vaultToken, fileKeyValueSeparator,
+	}, nil
 }
