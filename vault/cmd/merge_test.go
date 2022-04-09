d561c65b21ed101f40a416681068e36f3d8fcead
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-09 04:41:17 -0300

    refactor: improve unit tests

--- vault/cmd/merge_test.go
+++ vault/cmd/merge_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/ralvescosta/secrets-manager/vault/pkg/vault"
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
@@ -20,6 +21,51 @@ func Test_NewMergeCmd_should_execute_mergeCmd_correctly(t *testing.T) {
 	err := mergeCmd.Execute()
 
 	if err != nil {
+		t.Error()
+	}
+}
+
+func Test_getVaultConfigs_should_return_err_when_flag_missing(t *testing.T) {
+	cmd := &cobra.Command{}
+
+	configs, err := getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "flag filePath is required" {
+		t.Error()
+	}
+	cmd.Flags().String("file-path", "path", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "wrong vault separator" {
+		t.Error()
+	}
+	cmd.Flags().String("vault-separator", "separator", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "wrong path key value separator" {
+		t.Error()
+	}
+	cmd.Flags().String("path-key-value-separator", "separator", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "wrong kv version" {
+		t.Error()
+	}
+	cmd.Flags().String("kv-version", "version", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "wrong vault host" {
+		t.Error()
+	}
+	cmd.Flags().String("vault-host", "host", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "flag token is required" {
+		t.Error()
+	}
+	cmd.Flags().String("token", "token", "")
+
+	configs, err = getVaultConfigs(cmd.Flags())
+	if configs != nil || err == nil || err.Error() != "wrong file key value separator" {
 		t.Error()
 	}
 }
@@ -36,51 +82,6 @@ func Test_NewMergeCmd_should_return_err_if_some_err_occur(t *testing.T) {
 	err := mergeCmd.Execute()
 
 	if err == nil {
-		t.Error()
-	}
-}
-
-func Test_getVaultConfigs_should_return_err_when_flag_missing(t *testing.T) {
-	flags := &pflag.FlagSet{}
-
-	configs, err := getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "flag filePath is required" {
-		t.Error()
-	}
-
-	flags.Set("file-path", "file")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "wrong vault separator" {
-		t.Error()
-	}
-
-	flags.Set("vault-separator", "separator")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "wrong path key value separator" {
-		t.Error()
-	}
-
-	flags.Set("path-key-value-separator", "separator")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "wrong kv version" {
-		t.Error()
-	}
-
-	flags.Set("key-version", "version")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "wrong vault host" {
-		t.Error()
-	}
-
-	flags.Set("vault-host", "host")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "flag token is required" {
-		t.Error()
-	}
-
-	flags.Set("token", "token")
-	configs, err = getVaultConfigs(flags)
-	if configs != nil && err == nil && err.Error() != "wrong file key value separator" {
 		t.Error()
 	}
 }
