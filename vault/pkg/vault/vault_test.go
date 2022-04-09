b33ee698a8c64a9d00abfa2297db6b55a5f1ce31
ralvescosta <rafael.rac.mg@gmail.com>
2022-04-09 07:37:44 -0300

    refactor: improve unit tests

--- vault/pkg/vault/vault_test.go
+++ vault/pkg/vault/vault_test.go
@@ -4,9 +4,12 @@ Copyright © 2022 Rafael Costa <rafael.rac.mg@gmail.com>
 */
 package vault
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-func Test_should_execute_runner_correctly(t *testing.T) {
+func Test_Runner_should_execute_correctly(t *testing.T) {
 	readEnvFile = func(cfs *Configs) (map[string][]*environment, error) {
 		return make(map[string][]*environment), nil
 	}
@@ -20,6 +23,50 @@ func Test_should_execute_runner_correctly(t *testing.T) {
 	err := Runner(&Configs{})
 
 	if err != nil {
+		t.Error()
+	}
+}
+
+func Test_Runner_should_return_err_if_some_err_occur_in_readEnvFile(t *testing.T) {
+	readEnvFile = func(cfs *Configs) (map[string][]*environment, error) {
+		return make(map[string][]*environment), errors.New("some error")
+	}
+	err := Runner(&Configs{})
+
+	if err == nil {
+		t.Error()
+	}
+}
+
+func Test_Runner_should_return_err_if_some_err_occur_in_getVaultSecrets(t *testing.T) {
+	readEnvFile = func(cfs *Configs) (map[string][]*environment, error) {
+		return make(map[string][]*environment), nil
+	}
+	getVaultSecrets = func(cfs *Configs, envs map[string][]*environment) error {
+		return errors.New("some error")
+	}
+
+	err := Runner(&Configs{})
+
+	if err == nil {
+		t.Error()
+	}
+}
+
+func Test_Runner_should_return_err_if_some_err_occur_in_updateEnvFile(t *testing.T) {
+	readEnvFile = func(cfs *Configs) (map[string][]*environment, error) {
+		return make(map[string][]*environment), nil
+	}
+	getVaultSecrets = func(cfs *Configs, envs map[string][]*environment) error {
+		return nil
+	}
+	updateEnvFile = func(cfs *Configs, envs map[string][]*environment) error {
+		return errors.New("some error")
+	}
+
+	err := Runner(&Configs{})
+
+	if err == nil {
 		t.Error()
 	}
 }
